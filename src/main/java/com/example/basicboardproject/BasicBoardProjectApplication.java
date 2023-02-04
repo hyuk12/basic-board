3943c177857f424784635545d0d1958ac8661a42
hyuk12 <hotman7025@naver.com>
2023-02-04 20:23:01 +0900

    #6 - 스프링 부트 프로젝트 시작
    - 스프링 웹
    - 데브툴즈
    - 액츄얼
    - 롬복

---
+++ src/main/java/com/example/basicboardproject/BasicBoardProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.basicboardproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicBoardProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BasicBoardProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/basicboardproject/BasicBoardProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.basicboardproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicBoardProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
